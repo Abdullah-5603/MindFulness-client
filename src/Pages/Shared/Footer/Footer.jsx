597c2226a0dd3646bc765fa27fcc0cbe265ede0c
Abu Abdullah <abdullah72308@gmail.com>
2023-06-07 17:14:14 +0600

    slider added

--- src/Pages/Shared/Footer/Footer.jsx
+++ src/Pages/Shared/Footer/Footer.jsx
@@ -11,10 +11,10 @@ const Footer = () => {
                 <a className="link link-hover">FAQ</a>
                 <a className="link link-hover">Pre-Orders List</a>
                 <a className="link link-hover">Shipping & Returns</a>
-                <a className="link link-hover">Contact Email : animagic@action.com</a>
+                <a className="link link-hover">Contact Email : MindFulness@action.com</a>
             </div>
             <div className='mx-10'>
-                <span className="footer-title">ABOUT ANIMAGIC</span>
+                <span className="footer-title">ABOUT MindFulness</span>
                 <a className="link link-hover">Premium Member</a>
                 <a className="link link-hover">About us</a>
                 <a className="link link-hover">Terms of Service</a>
@@ -34,7 +34,7 @@ const Footer = () => {
                 <a><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" className="fill-current"><path d="M9 8h-3v4h3v12h5v-12h3.642l.358-4h-4v-1.667c0-.955.192-1.333 1.115-1.333h2.885v-5h-3.808c-3.596 0-5.192 1.583-5.192 4.615v3.385z"></path></svg></a>
             </div>
             <div className="text-center md: md:left-auto md:right-auto md:absolute md:top-64 mx-auto md:inset-x-0">
-                <p>&copy; 2023 - All rights reserved by ANIMAGIC ACTION FIGURE Ltd</p>
+                <p>&copy; 2023 - All rights reserved by MINDFULNESS Ltd</p>
             </div>
         </footer>
     );
